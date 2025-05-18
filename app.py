f4f5516990a72897fe41035545403beb85a260ae
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-05-18 00:38:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    nombre = None
+def saludar_nombre():
+    nombre_ingresado = None
     if request.method == 'POST':
-        nombre = request.form['nombre']
-    return render_template('index.html', nombre=nombre)
+        nombre_ingresado = request.form['nombre']
+    return render_template('saludo.html', nombre=nombre_ingresado)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    
